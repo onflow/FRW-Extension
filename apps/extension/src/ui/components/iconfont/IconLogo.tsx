9c3a25db408a0b93a013ae7ea71da97d7d58aba0
zzggo <han.zhang@flowfoundation.org>
2025-07-16 13:28:46 +1000

    fixed: revert iconlogo
    Closes #1088

--- apps/extension/src/ui/components/iconfont/IconLogo.tsx
+++ apps/extension/src/ui/components/iconfont/IconLogo.tsx
@@ -1,40 +1,41 @@
-import { useTheme } from '@mui/material/styles';
 import React from 'react';
 
+import { getIconColor } from './helper';
+
 interface IconLogoProps {
-  width?: number;
-  height?: number;
-  color?: string[];
+  size?: number;
+  color?: string;
+  style?: React.CSSProperties;
+  [key: string]: any;
 }
 
-export const IconLogo: React.FC<IconLogoProps> = ({ width = 24, height = 24, color }) => {
-  const theme = useTheme();
+const DEFAULT_STYLE: React.CSSProperties = {
+  display: 'block',
+};
 
-  const getIconColor = (colors: string[] | undefined, index: number, defaultColor: string) => {
-    if (colors && colors[index]) {
-      return colors[index];
-    }
-    return defaultColor;
-  };
+const IconLogo: React.FC<IconLogoProps> = ({ size = 18, color, style: _style, ...rest }) => {
+  const style = _style ? { ...DEFAULT_STYLE, ..._style } : DEFAULT_STYLE;
 
   return (
-    <svg
-      width={width}
-      height={height}
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
+    <svg viewBox="0 0 1024 1024" width={size + 'px'} height={size + 'px'} style={style} {...rest}>
       <path
-        d="M12 2L2 7L12 12L22 7L12 2Z"
-        fill={getIconColor(color, 0, theme.palette.success.main)}
+        d="M46.4 0m176 0l0 0q176 0 176 176l0 672q0 176-176 176l0 0q-176 0-176-176l0-672q0-176 176-176Z"
+        fill={getIconColor(color, 0, '#41CC5D')}
       />
-      <path d="M2 17L12 22L22 17" fill={getIconColor(color, 1, theme.palette.darkGray.main)} />
-      <path d="M2 12L12 17L22 12" fill={getIconColor(color, 2, theme.palette.purple.main)} />
       <path
-        d="M12 2L2 7L12 12L22 7L12 2Z"
-        fill={getIconColor(color, 3, theme.palette.yellow.main)}
+        d="M46.4 848c0-97.2032 78.7968-176 176-176h467.2c97.2032 0 176 78.7968 176 176s-78.7968 176-176 176H222.4c-97.2032 0-176-78.7968-176-176z"
+        fill={getIconColor(color, 1, '#363636')}
+      />
+      <path
+        d="M46.4 848c0 97.2032 78.7968 176 176 176s176-78.7968 176-176v-176H222.4c-97.2032 0-176 78.7968-176 176z"
+        fill={getIconColor(color, 2, '#7678ED')}
+      />
+      <path
+        d="M728 92.8m81.458701 81.458701l85.984185 85.984185q81.458701 81.458701 0 162.917402l-85.984185 85.984185q-81.458701 81.458701-162.917402 0l-85.984185-85.984185q-81.458701-81.458701 0-162.917402l85.984185-85.984185q81.458701-81.458701 162.917402 0Z"
+        fill={getIconColor(color, 3, '#FFD500')}
       />
     </svg>
   );
 };
+
+export default IconLogo;
